536e91913940c789c1b8806a6c60a3483a880341
Tim Notter <timnotter@hotmail.ch>
2023-02-03 12:49:01 +0100

    Reorg

--- constants.hpp
+++
@@ -1,5 +0,0 @@
-
-double lightYear = 9.4607e15;
-double massReference = 8.26e36;                     //Mass of Sagittarius A
-double lengthReference = 25000 * lightYear;         //Approximate size of milky way
-double timeReference = 1;
